a05e91e9460faa748e1787ee6e383923e7e56dc9
supan <supan@aci-bd.com>
2018-08-19 15:21:20 +0600

    A simple List to Grid and Grid to List Switch first commit...

---
+++ README.md
@@ -0,0 +1,9 @@
+ListToGrid Animation
+====================
+
+A simple List to Grid and Grid to List Switch Sample in android.
+
+## Requirements
+
+- Android API level 16 or greater
+- Android studio 3.0+
---
+++ app/src/main/java/com/sopan/list_to_grid_anim/SimpleAdapter.java
@@ -0,0 +1,32 @@
+package com.sopan.list_to_grid_anim;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder>{
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 30;
+    }
+
+    class SimpleViewHolder extends RecyclerView.ViewHolder {
+
+        SimpleViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
